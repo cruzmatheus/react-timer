8de86064d6db383c9935b9a8dfd48434b47e68d7
Matheus Cruz <contato.mscruz@gmail.com>
2022-07-07 12:22:08 +0100

    Refactor to use function components

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,19 +1,16 @@
 import React from 'react'
 import style from './Button.module.scss'
 
-class Button extends React.Component<{
-  children: string,
+interface Props {
+  children?: React.ReactNode,
   type?: "button" | "submit" | "reset" | undefined,
   onClick?: () => void
-}> {
-    render() {
-      const { type = "button", onClick } = this.props
-      return (
-        <button onClick={onClick} type={type} className={style.botao}>
-          {this.props.children}
-        </button>
-      )
-    }
 }
 
-export default Button
+export default function Button({ onClick, type, children }: Props) {
+  return (
+    <button onClick={onClick} type={type} className={style.botao}>
+      {children}
+    </button>
+  )
+}
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,74 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ITask } from '../../types/ITask'
 import Button from '../Button'
 import style from './Form.module.scss'
 import { v4 as uuidv4 } from 'uuid'
 
-class Form extends React.Component<{
+interface Props {
   setTasks: React.Dispatch<React.SetStateAction<ITask[]>>
-}> {
+}
 
-  state: ITask = {
-    id: uuidv4(),
-    name: "",
-    time: "00:00:00",
-    selected: false,
-    completed: false
-  }
-
-  addTask(event: React.FormEvent<HTMLFormElement>) {
+export default function Form({ setTasks }: Props) {
+  const [name, setName] = useState('')
+  const [time, setTime] = useState('00:00')
+  function addTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault()
-    this.props.setTasks(oldTasks => 
+    setTasks(oldTasks => 
       [...oldTasks, {
-          ...this.state,
+          name,
+          time,
           selected: false,
           completed: false,
           id: uuidv4()
         }
       ])
-    this.setState({
-      name: "",
-      time: "00:00"
-    })
+    setName('')
+    setTime('00:00')
   }
-  
-  render() {
-    return (
-      <form 
-        className={style.novaTarefa}
-        onSubmit={this.addTask.bind(this)}
-      >
-        <div className={style.inputContainer}>
-          <label htmlFor='task'>Add a new study subject</label>
-          <input 
-            type="text"
-            name='task'
-            id='task'
-            value={this.state.name}
-            onChange={event => this.setState({...this.state, name: event.target.value})}
-            placeholder='What do you want to study?'
-            required
-          />
-        </div>
-        <div className={style.inputContainer}>
-          <label htmlFor="time">Time</label>
-          <input
-            type='time'
-            step='1'
-            name='time'
-            id='time'
-            value={this.state.time}
-            onChange={event => this.setState({...this.state, time: event.target.value})}
-            min='00:00:00'
-            max='01:30:00'
-            required />
-        </div>
-        <Button type="submit">
-          Add
-        </Button>
-      </form>
-    )
-  }
-}
 
-export default Form
+  return (
+    <form 
+      className={style.novaTarefa}
+      onSubmit={addTask}
+    >
+      <div className={style.inputContainer}>
+        <label htmlFor='task'>Add a new study subject</label>
+        <input 
+          type="text"
+          name='task'
+          id='task'
+          value={name}
+          onChange={event => setName(event.target.value)}
+          placeholder='What do you want to study?'
+          required
+        />
+      </div>
+      <div className={style.inputContainer}>
+        <label htmlFor="time">Time</label>
+        <input
+          type='time'
+          step='1'
+          name='time'
+          id='time'
+          value={time}
+          onChange={event => setTime(event.target.value)}
+          min='00:00:00'
+          max='01:30:00'
+          required />
+      </div>
+      <Button type="submit">
+        Add
+      </Button>
+    </form>
+  )
+}
